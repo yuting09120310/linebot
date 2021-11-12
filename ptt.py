a1beefa2b5dfce1fab5d3bd13d0331066e2c8e12
yuting <yuting09120310@gmail.com>
2021-11-12 12:52:18 +0800

    123

--- ptt.py
+++ ptt.py
@@ -2,8 +2,9 @@ def function_ptt():
     import requests
     from bs4 import BeautifulSoup
     res = requests.get("https://www.ptt.cc/bbs/MobileComm/index.html")
-    soup = BeautifulSoup(res.text)
+    soup = BeautifulSoup(res.text,"html.parser")
     for item in soup.select('.r-ent'):
         message = item.select('.title')[0].text
-    return message
-    
+    print(message)
+
+function_ptt()
