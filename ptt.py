cd204be6408ef7113bc6503d516f2061143799c7
yuting <yuting09120310@gmail.com>
2021-11-12 14:25:59 +0800

    3c 推送

--- ptt.py
+++ ptt.py
@@ -9,14 +9,11 @@ import json
 
 def ticketInfo():
     inFo = ""
-    resp = requests.get('https://www.ptt.cc/bbs/Japan_Travel/index.html')
+    resp = requests.get('https://www.ptt.cc/bbs/MobileComm/index.html')
     soup = BeautifulSoup(resp.text, 'html.parser')
     main_titles = soup.find_all('div', 'title')
 
     for title in main_titles:
-
-        if "資訊" in title.text:
-            inFo += title.text.strip() + "\n"
-            inFo += "https://www.ptt.cc" + title.find("a")['href'] + "\n"
-
+        inFo += title.text.strip() + "\n"
+        inFo += "https://www.ptt.cc" + title.find("a")['href'] + "\n"
     return inFo
