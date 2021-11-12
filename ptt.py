7017652c5c969a31b61b853039c536b3cebc82b0
yuting <yuting09120310@gmail.com>
2021-11-12 13:01:44 +0800

    推

--- ptt.py
+++ ptt.py
@@ -1,8 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
 def function_ptt():
-    import requests
-    from bs4 import BeautifulSoup
-    res = requests.get("https://www.ptt.cc/bbs/MobileComm/index.html")
-    soup = BeautifulSoup(res.text,"html.parser")
-    for item in soup.select('.r-ent'):
-        message = item.select('.title')[0].text
-    return message
+    message = TextSendMessage(
+        text="是否註冊成為會員？",
+    #     res = requests.get("https://www.ptt.cc/bbs/MobileComm/index.html")
+    #     soup = BeautifulSoup(res.text,"html.parser")
+    #     for item in soup.select('.r-ent'):
+    #         message = item.select('.title')[0].text
+     )
+    print(message) 
