15ed08c62eea0986bfca9bf8e6d2a58a529103f4
yuting <yuting09120310@gmail.com>
2021-11-12 10:54:58 +0800

    推送

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
+    if 'Alex作品集' in msg:
+        message = "https://yuting09120310.github.io/Alex_Blog/"
         line_bot_api.reply_message(event.reply_token, message)
     elif '最新活動訊息' in msg:
         message = buttons_message()
