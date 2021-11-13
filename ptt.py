c6b106d44d0338e609f9c34778252c288915d23c
alex <yuting09120310@hotmail.com>
2021-11-13 16:04:41 +0800

    用錯函數

--- ptt.py
+++ ptt.py
@@ -8,12 +8,14 @@ import psycopg2
 import json
 
 def ticketInfo(keyword):
+    data = keyword.split(' ')
+
     inFo = ""
     resp = requests.get('https://www.ptt.cc/bbs/MobileComm/index.html')
     soup = BeautifulSoup(resp.text, 'html.parser')
     main_titles = soup.find_all('div', 'title')
 
-    data = keyword.split(' ')
+    
 
     if(len(data) > 1):
         for title in main_titles:
@@ -23,16 +25,18 @@ def ticketInfo(keyword):
     else:
         for title in main_titles:
             if "本文已被刪除" in title.text:
-                break
-            inFo += title.text.strip() + "\n"
-            inFo += "https://www.ptt.cc" + title.find("a")['href'] + "\n"
+                pass
+            else:
+                inFo += title.text.strip() + "\n"
+                inFo += "https://www.ptt.cc" + title.find("a")['href'] + "\n"
 
     print(inFo)
     return inFo
     
 # def test():
-#     str = "Ptt"
-#     str.lower()
-#     print(str)
-
+#     str = "ptt 3c iphone12" 
+#     data = str.split(' ')
+#     print(len(data))
 # test()
+
+ticketInfo("ptt")
