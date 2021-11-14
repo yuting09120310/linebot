c37a6be674f6e56c1d8702c290f219394016b28b
yuting <yuting09120310@gmail.com>
2021-11-15 03:42:53 +0800

    修改

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from linebot.models import *
 from message import *
 from new import *
 from Function import *
-from ptt import ticketInfo
+from ptt import ticketInfo, dcard
 #======這裡是呼叫的檔案內容=====
 
 #======python的函數庫==========
@@ -75,6 +75,10 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
     elif 'ptt' in user_keyword:
         outInfo += ticketInfo(msg)
+        message = TextSendMessage(text=outInfo)
+        line_bot_api.reply_message(event.reply_token,message)
+    elif 'dcard' in user_keyword:
+        outInfo += dcard()
         message = TextSendMessage(text=outInfo)
         line_bot_api.reply_message(event.reply_token,message)
     else:
