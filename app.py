fdf47aec473dd33de6d96632442989921fb63264
yuting <yuting09120310@gmail.com>
2021-11-15 03:36:11 +0800

    回復

--- app.py
+++ app.py
@@ -70,12 +70,15 @@ def handle_message(event):
     # elif '功能列表' in msg:
     #     message = function_list()
     #     line_bot_api.reply_message(event.reply_token, message)
+    elif 'Alex' in user_keyword:
+        message = function_list()
+        line_bot_api.reply_message(event.reply_token, message)
     elif 'ptt' in user_keyword:
-        outInfo += ticketInfo(msg)
+        outInfo += ticketInfo(user_keyword)
         message = TextSendMessage(text=outInfo)
         line_bot_api.reply_message(event.reply_token,message)
     elif 'dcard' in user_keyword:
-        outInfo += dcard()
+        outInfo += test()
         message = TextSendMessage(text=outInfo)
         line_bot_api.reply_message(event.reply_token,message)  
     else:
