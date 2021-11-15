50855685541c9f6782513547b16d78ee6f1e7509
alex <yuting09120310@hotmail.com>
2021-11-15 17:28:47 +0800

    修改

--- ptt.py
+++ ptt.py
@@ -33,23 +33,59 @@ def ticketInfo(keyword):
 
     print(inFo)
     return inFo
-    
-def dcard():
+
+def stock(keyword):
     inFo = ""
+    data = keyword.split(' ')
     headers = {
         'user-agent': 'Mozilla/5.0'
     }
-    proxy = {
-    'HTTPS': '59.124.224.205:3128'
-    }
-    resp = requests.get('https://www.dcard.tw/f/pet', headers = headers , proxies=proxy)
+    resp = requests.get('https://tw.stock.yahoo.com/quote/' + data[1] + '.TW', headers = headers)
     soup = BeautifulSoup(resp.text, 'html.parser')
-    main_titles = soup.find_all('a',href=re.compile(r'pet/p/'))
-    for data in main_titles:
-        inFo += data.text + "\n"
-        url = "https://www.dcard.tw" + data.get('href')
-        inFo += url + "\n"
+    main_titles = soup.find_all('h2',re.compile(r'Fz'))
+    inFo += main_titles[0].text + '\n'
+    main_titles = soup.find_all('span',re.compile(r'Fz'))
+    inFo += main_titles[1].text + '\n'
+    inFo += main_titles[2].text + '\n'
+    inFo += main_titles[9].text + '\n'
+
     print(inFo)
     return inFo
+    
+#代理問題
+# def dcard():
+#     inFo = ""
+#     headers = {
+#         'user-agent': 'Mozilla/5.0'
+#     }
+#     proxy = {
+#     'http': '18.179.120.38:80'
+#     }
+#     resp = requests.get('https://www.dcard.tw/f/pet', headers = headers, proxies = proxy)
+#     soup = BeautifulSoup(resp.text, 'html.parser')
+#     main_titles = soup.find_all('a',href=re.compile(r'pet/p/'))
+#     for data in main_titles:
+#         inFo += data.text + "\n"
+#         url = "https://www.dcard.tw" + data.get('href')
+#         inFo += url + "\n"
+#     print(inFo)
+#     return inFo
 
-dcard()
+#以下字典
+# def dec():
+#     inFo = ""
+#     headers = {
+#         'user-agent': 'Mozilla/5.0'
+#     }
+#     resp = requests.get('https://blog.amazingtalker.com/zh-tw/zh-eng/%E5%9C%8B%E9%AB%98%E4%B8%AD%E5%BF%85%E8%83%8C8000%E5%96%AE%E5%AD%97%E8%A1%A8/3747/', headers = headers)
+#     soup = BeautifulSoup(resp.text, 'html.parser')
+#     main_titles = soup.find_all('td',re.compile(r'tg-yw4l'))
+#     path = 'output.txt'
+#     f = open(path, 'w' ,encoding="utf-8")
+
+#     for item in main_titles:
+#         f.write(item.text + '\n')
+#     f.close()
+
+    
+    
