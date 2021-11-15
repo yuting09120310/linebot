f9b5ffc00cd6e938a7b02eac48b62e1cd8cb7463
alex <yuting09120310@hotmail.com>
2021-11-15 18:16:10 +0800

    推送

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def handle_message(event):
     outInfo = ''
 
     if(user_keyword.isdigit()):
-        msg = "股票 " + msg
+        user_keyword = "股票 " + msg
 
     if '最新合作廠商' in user_keyword:
         message = imagemap_message()
