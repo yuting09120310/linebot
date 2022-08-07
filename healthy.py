b8e67f5d27ef0189fdc2d5d0b50c4ed1f5a7d28f
alex <yuting09120310@gmail.com>
2022-08-08 00:47:07 +0800

    紀錄狀態

--- healthy.py
+++ healthy.py
@@ -7,36 +7,37 @@ from urllib import parse, request
 import requests
 from bs4 import BeautifulSoup
 import pymysql
+import time
 
-def record(keyword):
 
-    db_settings = {
-        "host":"us-cdbr-east-06.cleardb.net",          # 主機名稱
-        "db":"heroku_8760c94dd464d66", # 資料庫名稱
-        "user":"b51153f5c0e98d",        # 帳號
-        "password":"11bb074c",   # 密碼
+db_settings = {
+        "host":"us-cdbr-east-06.cleardb.net",
+        "db":"heroku_8760c94dd464d66",
+        "user":"b51153f5c0e98d",
+        "password":"11bb074c",
         "port": 3306,
         "charset": "utf8"
     }
 
+def record(keyword):
+    data = keyword.split(' ')
+
+    # 抓當下時間
+    t = time.localtime()
+    result = time.strftime("%Y-%m-%d", t)
+
     try:
+        
         conn = pymysql.connect(**db_settings)
 
         cursor = conn.cursor()
+
         command = "INSERT INTO record (name, high_mmHg, low_mmHg, pulse, time) VALUES (%s,%s,%s,%s,%s)"
-        # 取得華語單曲日榜
-        cursor.execute(command, ("alex", "100", "63" , "56" , "2022-08-08"))
-                
-            # 儲存變更
+        cursor.execute(command, (data[0], data[1], data[2] , data[3] , result))
+    
         conn.commit()
             
-
-        print("連線成功")
+        print("紀錄完成")
 
     except Exception as ex:
-        print(ex)
-
-
-
-data = input()
-record(data)
+        print("紀錄失敗")
