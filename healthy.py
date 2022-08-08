3c4bde3bf1dc99b1c13ff0254f3eb28f8f8f71d8
alex <yuting09120310@gmail.com>
2022-08-08 22:30:34 +0800

    排版測試

--- healthy.py
+++ healthy.py
@@ -53,12 +53,12 @@ def show(keyword):
     cursor.execute(command)
 
     result = cursor.fetchall()
-    inFo = "     時間        最高血壓     最低血壓      脈搏" + "\n"
+    inFo = "時間     最高血壓    最低血壓   脈搏" + "\n"
     result = list(result)
     for item in result:
-        inFo += ("----------------------------------------------------") + "\n"
-        inFo += ("| " +str(item[0]) + "  |    " + str(item[1]) + "     |     " + str(item[2]) + "     |    " + str(item[3]) + "    |") + "\n"
+        inFo += ("---------------------------------------") + "\n"
+        inFo += ("| " +str(item[0]) + "| " + str(item[1]) + " | " + str(item[2]) + " |" + str(item[3]) + "|") + "\n"
     
-    inFo += ("----------------------------------------------------") + "\n"
+    inFo += ("---------------------------------------") + "\n"
     print(inFo)
     return inFo
